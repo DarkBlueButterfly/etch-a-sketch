8d82a328d0b8e2d6a92eb8cc5aea283e8a2c6a75
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-05-29 09:51:41 -0600

    Add HTML, javascript and CSS files
    Created a 16x16 grid of square divs using JavaScript and Flexbox.

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// console.log("hello world");
+const container = document.querySelector("#container");
+
+for(i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        container.appendChild(grid);
+    }
+}
