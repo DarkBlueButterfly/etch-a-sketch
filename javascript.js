69869cf2288aa8038213d07390acc6420004becb
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-05-29 16:41:31 -0600

    Set limit to user input maximum

--- javascript.js
+++ javascript.js
@@ -18,10 +18,54 @@ btn.addEventListener("click", gridSize);
 
 function gridSize() {
     let input = prompt("How large do you want your new grid? (1 - 100)");
-    input = Number(input);
-    // console.log(typeof input);
-    while (input > 100 || input <= 0) {
-        input = Number(prompt("Please choose between 1 and 100"));
+    input = parseInt(input);
+    while (input === input) {
+        if (input > 100 || input <= 0) {
+            input = parseInt(prompt("Please choose between 1 and 100"));
+        } else {
+            break;
+        }
     }
 }
+
+// function gridSize() {
+//     let input = prompt("How large do you want your new grid? (1 - 100)");
+//     input = Number(input);
+//     while (true) {
+//     if (input > 100 || input <= 0) {
+//         input = Number(prompt("Please choose between 1 and 100"));
+//     } 
+//     }
+    // const grid = document.querySelectorAll("grid");
+    // grid.container.removeChild();
+    // return input;
+    // for(i = 0; i < input; i++) {
+    //     for(j = 0; j < input; j++) {
+    //         const grid = document.createAttribute("div");
+    //         grid.classList.add("grid");
+    //         container.appendChild(grid);
+    //     }
+    // }
+
+//}
 // gridSize() //check
+
+//clear all previous divs
+// function clearDiv() {
+//     grid.remove();
+// }
+
+// const grid = document.querySelectorAll("div");
+//     grid.parentNode.removeChild();
+
+//new grid
+function newGrid() {
+    // input = gridSize();
+    for(i = 0; i < input; i++) {
+        for(j = 0; j < input; j++) {
+            const grid = document.createAttribute("div");
+            grid.classList.add("grid");
+            container.appendChild(grid);
+        }
+    }
+}
