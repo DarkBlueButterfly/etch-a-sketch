2d25f764e0048d002c778ba32a00107717a8682f
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-05-29 19:27:56 -0600

    Add dynamic grid size

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
 // console.log("hello world");
 const container = document.querySelector("#container");
 
-for(i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        container.appendChild(grid);
+function setUp(initialSize) {
+    const square = document.querySelectorAll(".grid");
+    square.forEach((div) => div.remove());
+
+    for(i = 0; i < initialSize; i++) {
+        for (let j = 0; j < initialSize; j++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            container.appendChild(grid);
+        }
     }
 }
+setUp(16); // 16x16 grid
 
 container.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "red";
@@ -26,7 +32,11 @@ function gridSize() {
             break;
         }
     }
+    // return input;
+    setUp(input);
 }
+
+// console.log(gridSize());
 
 // function gridSize() {
 //     let input = prompt("How large do you want your new grid? (1 - 100)");
@@ -59,13 +69,13 @@ function gridSize() {
 //     grid.parentNode.removeChild();
 
 //new grid
-function newGrid() {
-    // input = gridSize();
-    for(i = 0; i < input; i++) {
-        for(j = 0; j < input; j++) {
-            const grid = document.createAttribute("div");
-            grid.classList.add("grid");
-            container.appendChild(grid);
-        }
-    }
-}
+// function newGrid() {
+//     // input = gridSize();
+//     for(i = 0; i < input; i++) {
+//         for(j = 0; j < input; j++) {
+//             const grid = document.createAttribute("div");
+//             grid.classList.add("grid");
+//             container.appendChild(grid);
+//         }
+//     }
+// }
